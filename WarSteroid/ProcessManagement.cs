44565f1ae518a15836d66c632b5aa24539c80f37
dayoffsofia <slavov17@gmail.com>
2020-10-13 23:35:58 +0300

    Add project files.

---
+++ WarSteroid/ProcessManagement.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Jupiter;
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Jupiter;
+using Microsoft.Win32.SafeHandles;
+using System.Linq;
+
+namespace WarSteroid
+{
+    class ProcessManagement
+    {
+        [DllImport("kernel32.dll")]
+        internal static extern bool IsWow64Process(SafeProcessHandle processHandle, out bool isWow64Process);
+
+
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr OpenProcess(uint processAccess, bool bInheritHandle, int processId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out IntPtr lpNumberOfBytesRead);
+
+        public static IntPtr FindDMAAddy(IntPtr hProc, IntPtr ptr, int[] offsets)
+        {
+            var buffer = new byte[IntPtr.Size];
+
+            foreach (int i in offsets)
+            {
+                ReadProcessMemory(hProc, ptr, buffer, buffer.Length, out var read);
+
+                ptr = (IntPtr.Size == 4)
+                ? IntPtr.Add(new IntPtr(BitConverter.ToInt32(buffer, 0)), i)
+                : ptr = IntPtr.Add(new IntPtr(BitConverter.ToInt64(buffer, 0)), i);
+            }
+            return ptr;
+        }
+
+       
+    public static IntPtr GetModuleBaseAddress(string processName, string moduleName)
+        {
+            // Get an instance of the specified process
+            Process process;
+
+            try
+            {
+                process = Process.GetProcessesByName(processName)[0];
+            }
+
+            catch (IndexOutOfRangeException)
+            {
+                // The process isn't currently running
+                throw new ArgumentException($"No process with name {processName} is currently running");
+            }
+
+            // Get an instance of the specified module in the process
+            // We use linq here to avoid unnecesary for loops
+            Console.WriteLine("Searching....");
+            var module = process.Modules.Cast<ProcessModule>().SingleOrDefault(m => string.Equals(m.ModuleName, moduleName, StringComparison.OrdinalIgnoreCase));
+
+            // Attempt to get the base address of the module - Return IntPtr.Zero if the module doesn't exist in the process
+            Console.WriteLine("Found!");
+            return module?.BaseAddress ?? IntPtr.Zero;
+        }
+    }
+}
---
+++ WarSteroid/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Linq;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Jupiter;
+using Microsoft.Win32.SafeHandles;
+namespace WarSteroid
+{
+    class Program
+    {
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(long dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory2(IntPtr hProcess,
+            long lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(IntPtr hProcess,
+            Int64 lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+        [DllImport("kernel32.dll")]
+        static extern bool WriteProcessMemory(int hProcess, int lpBaseAddress,
+            byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesWritten);
+
+        const int PROCESS_WM_READ = 0x0010;
+        const int PROCESS_VM_WRITE = 0x0020;
+        const int PROCESS_VM_OPERATION = 0x0008;
+        static void Main()
+        {
+            Console.WriteLine("starting");
+            var wc3baseaddr = ProcessManagement.GetModuleBaseAddress("Warcraft III","Warcraft III.exe");
+            Process process = Process.GetProcessesByName("Warcraft III")[0];
+            IntPtr processHandle = OpenProcess(PROCESS_WM_READ, false, process.Id);
+            string hexValue = wc3baseaddr.ToString("X");
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024]; //To read a 24 byte unicode string
+
+
+            ReadProcessMemory(processHandle, 0x167652EFAB8, buffer, buffer.Length, ref bytesRead);
+
+            string data = Encoding.ASCII.GetString(buffer);
+            string hex_str = BitConverter.ToString(buffer);
+
+
+            Console.WriteLine(Encoding.ASCII.GetString(buffer) +" (" + bytesRead.ToString() + "bytes)");
+            Console.WriteLine(BitConverter.ToString(buffer) +" (" + bytesRead.ToString() + "bytes)");
+
+           
+            
+        }
+
+        public static void RAMReader(IntPtr BaseAddress)
+        {
+
+         
+            //int bytesRead = 0;
+            //byte[] buffer = new byte[4];
+
+            //ReadProcessMemory( BaseAddress, 0x167652EFAB8, buffer, buffer.Length, ref bytesRead);
+            //Console.WriteLine(buffer);
+
+
+        }
+
+
+        
+    }
+}
